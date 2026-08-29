6a851132c874f473086b2bdc863da4e94ff7c1b8
Chetana Kupati <chaitanakupati@gmail.com>
2026-08-29 22:42:10 +0530

    Fix database configuration and migrations

--- student_management/settings.py
+++ student_management/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     BASE_DIR / 'students' / 'static',
 ]
