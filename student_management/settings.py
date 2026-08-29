86ce302fc44fd2ca5400fddb313d1012f7b967c1
Chetana Kupati <chaitanakupati@gmail.com>
2026-08-29 21:29:36 +0530

    Connect Django to PostgreSQL

--- student_management/settings.py
+++ student_management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,10 +86,9 @@ WSGI_APPLICATION = 'student_management.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
 
